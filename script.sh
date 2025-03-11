644477869a0481d1f1209dcc206c1134ad46b51f
Cristian <cralonsh8@gmail.com>
2025-03-11 10:31:41 +0100

    Script updated with AWS and Terraform CLI prerequisite verification

--- script.sh
+++ script.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+# Verify AWS CLI prerequisite
+aws_path="/usr/local/bin/aws"
+if [ ! -x ${aws_path} ]
+then
+    eval $(curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip")
+    eval $(unzip awscliv2.zip)
+    eval $(./aws/install)
+else
+    echo "[INFO]: AWS CLI Already installed"
+fi
+
+# Verify Terraform CLI prerequisite
+terraform_path="/usr/local/bin/terraform"
+if [ ! -x ${terraform_path} ]
+then
+    eval $(wget https://releases.hashicorp.com/terraform/1.11.1/terraform_1.11.1_linux_amd64.zip)
+    eval $(unzip terraform_1.11.1_linux_amd64.zip)
+    eval $(mv terraform/ ${terraform_path})
+    eval $(terraform -install-autocomplete)
+else
+    echo "[INFO]: Terraform CLI Already installed"
+fi
 # Sets Terraform folder structure
 folder_list=("terraform" "terraform/dev" "terraform/test" "terraform/prod" "terraform/modules")
 
@@ -9,7 +31,7 @@ do
     then
         mkdir "${HOME}/${folder}"
     else
-        echo "${HOME}/${folder} already exists"
+        echo "[INFO]: ${HOME}/${folder} already exists"
     fi
 done
 
@@ -22,6 +44,6 @@ then
     eval $(aws s3 mb s3://${bucket} --region ${region})
     eval $(aws s3api put-bucket-versioning --bucket ${bucket} --versioning-configuration Status=Enabled)
 else
-    echo "${bucket} already exists"
+    echo "[INFO]: ${bucket} already exists"
 fi
 
