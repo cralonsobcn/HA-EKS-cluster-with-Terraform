391ee440cdeb73b7ec335d6c1afd36c1e9f9826c
Cristian <cralonsh8@gmail.com>
2025-03-11 21:10:52 +0100

    Updated CIDR ranges to comply with IPv4 VPC CIDR blocks

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -8,13 +8,33 @@ variable "aws_region" {
 
 ## ( -- Networking -- )
 variable "vpc-name" { # TODO
-    default = "pipeline-tf-aws-vpc"
-    description = "Name assigend to the VPC"
+  type = string
+  default = "pipeline-tf-aws-vpc"
+  description = "Name assigend to the VPC"
 }
 
 variable "vpc-cidr" {
-  default = "200.200.0.0/24"
+  type = string
+  default = "10.0.0.0/16"
   description = "CIDR block assigned to the VPC"
+}
+
+variable "subnet-a-cidr" {
+  type = string
+  default = "10.0.1.0/24"
+  description = "CIDR block assigned to the subnet aws-vpc-subnet-a"
+}
+
+variable "subnet-b-cidr" {
+  type = string
+  default = "10.0.2.0/24"
+  description = "CIDR block assigned to the subnet aws-vpc-subnet-b"  
+}
+
+variable "subnet-c-cidr" {
+   type = string
+  default = "10.0.3.0/24"
+  description = "CIDR block assigned to the subnet aws-vpc-subnet-c" 
 }
 
 ## ( -- AWS CodePipeline -- )
