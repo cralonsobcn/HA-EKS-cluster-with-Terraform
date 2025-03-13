67f71323f9127373c23a6dec56e5530ea96c91f0
Cristian <cralonsh8@gmail.com>
2025-03-13 15:27:20 +0100

    Added EKS Cluster and EKS Node Group IAM resources. Initial commit of Codepipeline IAM resources

--- terraform/dev/iam.tf
+++ terraform/dev/iam.tf
@@ -1,20 +1,18 @@
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role#attribute-reference
-resource "aws_iam_role" "eksClusterRole" {
-  name               = var.eks-cluster-role-name
-  assume_role_policy = data.aws_iam_policy_document.policy-role-eks.json
+## ( -- IAM EKS Cluster -- ) ##
+resource "aws_iam_role" "eks-cluster-role" {
+  name               = var.eks-cluster-name
+  assume_role_policy = data.aws_iam_policy_document.eks-cluster-policy-role.json # Retrieves the policy docuemnt
   tags = {
-    Name = var.eks-cluster-role-name
+    Name = var.eks-cluster-name
   }
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_user_policy_attachment
-resource "aws_iam_role_policy_attachment" "eksClusterRole_AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eksClusterRole.name
+resource "aws_iam_role_policy_attachment" "eks-cluster-role-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy" # Retrives the specific EKS policy 
+  role       = aws_iam_role.eks-cluster-role.name
 }
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document
-data "aws_iam_policy_document" "policy-role-eks" {
+data "aws_iam_policy_document" "eks-cluster-policy-role" { 
   statement {
     effect = "Allow"
 
@@ -25,4 +23,100 @@ data "aws_iam_policy_document" "policy-role-eks" {
 
     actions = ["sts:AssumeRole"]
   }
+}
+
+## ( -- IAM EKS Node Group -- ) ##
+resource "aws_iam_role" "eks-nodeGroup-role" {
+  name = var.eks-nodeGroup-name
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks-nodeGroup-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks-nodeGroup-name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-nodeGroup-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks-nodeGroup-name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-nodeGroup-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks-nodeGroup
+}
+
+## ( -- IAM CodePipeline -- ) ## TODO
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name               = "test-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "codepipeline_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "s3:PutObjectAcl",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      aws_s3_bucket.codepipeline_bucket.arn,
+      "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+    ]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["codestar-connections:UseConnection"]
+    resources = [aws_codestarconnections_connection.github-repo.arn]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "codebuild:BatchGetBuilds",
+      "codebuild:StartBuild",
+    ]
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name   = "codepipeline_policy"
+  role   = aws_iam_role.codepipeline_role.id
+  policy = data.aws_iam_policy_document.codepipeline_policy.json
+}
+
+data "aws_kms_alias" "s3kmskey" {
+  name = "alias/myKmsKey"
 }
