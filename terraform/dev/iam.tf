49a4c7d8f046bb65948d5f1f7cfda030a7febbba
Cristian <cralonsh8@gmail.com>
2025-03-14 18:31:47 +0100

    Removed nodegroup blocks

--- terraform/dev/iam.tf
+++ terraform/dev/iam.tf
@@ -25,37 +25,6 @@ data "aws_iam_policy_document" "eks-cluster-policy-role" {
   }
 }
 
-## ( -- IAM EKS Node Group -- ) ##
-resource "aws_iam_role" "eks-nodeGroup-role" {
-  name = var.eks-nodeGroup-name
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "eks-nodeGroup-AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks-nodeGroup-role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks-nodeGroup-AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks-nodeGroup-role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks-nodeGroup-AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks-nodeGroup-role.name
-}
-
 ## ( -- IAM CodePipeline -- ) ## TODO
 data "aws_iam_policy_document" "assume_role" {
   statement {
