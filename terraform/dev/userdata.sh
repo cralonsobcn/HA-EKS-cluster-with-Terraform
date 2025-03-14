752303ad1d009e6d3dca21a632b65e3ef71d5e98
Cristian <cralonsh8@gmail.com>
2025-03-14 18:37:26 +0100

    Added userdata associated with the dataplane-node-template

---
+++ terraform/dev/userdata.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# container runtime
+dnf install -y docker
+systemctl --now enable docker
+
+# kubectl
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/repodata/repomd.xml.key
+exclude=kubelet kubectl kubeadm cri-tools kubernetes-cni
+EOF
+sudo dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+dnf install bash-completion
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+echo 'alias k=kubectl' >>~/.bashrc
+echo 'complete -o default -F __start_kubectl k' >>~/.bashrc
+systemctl enable --now kubelet
