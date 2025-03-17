0e162e5db80eceb466e929ec07ee60df9513b97a
Cristian <cralonsh8@gmail.com>
2025-03-17 17:25:06 +0100

    Switch to Kubernetes 1.32. Switch from private VPC to default VPC as per Kodekloud requirements

--- terraform/dev/controlplane.tf
+++ terraform/dev/controlplane.tf
@@ -1,23 +1,41 @@
 resource "aws_eks_cluster" "eks-cluster" {
   name = var.eks-cluster-name
+  
+  # Disable EKS Auto Mode
+  compute_config {
+    enabled = false
+  }
+  kubernetes_network_config {
+    elastic_load_balancing {
+      enabled = false
+    }
+  }
+  storage_config {
+    block_storage {
+      enabled = false
+    }
+  }
 
+  # Sets access configuration to API and configMap
   access_config {
     authentication_mode = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
   }
 
   role_arn = aws_iam_role.eks-cluster-role.arn
-  version  = "1.31"
+  version  = "1.32"
 
+  # Sets the subnets to be used by the EKS Controlplane
   vpc_config {
-    subnet_ids = [aws_subnet.aws-vpc-controlplane-subnet-a.id, aws_subnet.aws-vpc-controlplane-subnet-b.id]
+    subnet_ids = [aws_default_subnet.aws-vpc-controlplane-subnet-a.id, aws_default_subnet.aws-vpc-controlplane-subnet-b.id]
   }
 
-  # Ensure that IAM Role permissions are created before and deleted
-  # after EKS Cluster handling. Otherwise, EKS will not be able to
-  # properly delete EKS managed EC2 infrastructure such as Security Groups.
-  depends_on = [aws_iam_role_policy_attachment.eks-cluster-role-AmazonEKSClusterPolicy]
+  # Deploy the EKS Controlplane only if the VPC and the cluster role are created 
+  depends_on = [aws_iam_role.eks-cluster-role, aws_iam_role_policy_attachment.eks-cluster-role-AmazonEKSClusterPolicy, aws_default_vpc.aws-vpc-controlplane]
 
   tags = {
     Name = var.eks-cluster-name
   }
 }
+
+# aws eks update-kubeconfig --region region-code --name my-cluster
