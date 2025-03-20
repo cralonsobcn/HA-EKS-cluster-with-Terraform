b053412e1445c8f3db647537480b6c24e925d99d
Cristian <cralonsh8@gmail.com>
2025-03-20 13:05:46 +0100

    Deleted unwanted files

--- terraform/test/alt-script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Access key and secret Key for TERRAFOM AWS Provider 
-
-aws_user=$(aws iam get-user | jq -r '.User.UserName')
-aws_region="us-east-1"
-aws_output=$(aws iam create-access-key --user-name ${aws_user} --region ${aws_region}) 
-TF_VAR_AWS_ACCESS_KEY=$(echo "${aws_output}" | jq -r '.AccessKey.AccessKeyId') # Env
-TF_VAR_AWS_SECRET_KEY=$(echo "${aws_output}" | jq -r '.AccessKey.SecretAccessKey') # Env
